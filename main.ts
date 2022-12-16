f4ceaed342794d39c32a043ec654ff01d7d8fe70
Jakubpesser <115145147+Jakubpesser@users.noreply.github.com>
2022-12-16 11:26:40 +0100

    Aktualizovat pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
-basic.forever(function () {
-	
-})
+function getInput(){
+    return input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B) 
+
+    
+let wasPressed = false
+
+let score = 0
+
+while(true){
+    if (getInput()){
+        if (!wasPressed) {
+
+        }
+        wasPressed = true
+    }else{
+        if (wasPressed){
+
+        }
+        wasPressed = false
+    }
+}
